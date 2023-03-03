513b3f4b952d895ac6ce280d3fbe722379b355c1
nandlalyadav57 <33649510+nandlalyadav57@users.noreply.github.com>
2023-03-03 16:10:46 +0530

    update

---
+++ scripts/disable_all_cleanup_cronjobs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for NS in $(kubectl get ns --no-headers|grep astronomer |awk '{print $1}'); 
+  do 
+    for jobs in $(kubectl get cronjobs --no-headers -n $NS|grep cleanup|grep -v houston | awk '{print $1}') ; do
+      echo $jobs;kubectl patch cronjobs $jobs -p '{"spec" : {"suspend" : false }}' -n $NS
+    done
+done 
